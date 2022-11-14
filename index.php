f5a1e1c6d9ca98e8d8fe09130e5bef411e814895
6-ON <aminhatim6@gmail.com>
2022-11-14 23:52:07 +0100

    add some config files

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once "includes/etc/css-cdn.php"; ?>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"> -->
+
 </head>
+
 <body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <a name="" id="test" class="btn btn-primary" href="#" role="button">Button</a>
+    <footer>
+        <!-- place footer here -->
+    </footer>
     
+    <?php require_once "includes/etc/js-cdn.php" ?>
+
+    <script src="src/js/test.js"></script>
 </body>
+
 </html>
---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+
+$("#test").on("click", function() {
+   //test jquery
+})
+
+
