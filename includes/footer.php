4b952183b10d3c29e56f1d98a1bc9ab29b12d4b1
6-ON <aminhatim6@gmail.com>
2022-11-16 23:57:00 +0100

    add footer section

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<div class="container  ">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center pb-3 mb-3  ">
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+            <li class="nav-item"><a href="shop.php" class="nav-link px-2 text-muted">Products</a></li>
+            <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-muted">Contact</a></li>
+            <li class="nav-item"><a href="about-us.php" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+        <hr class="w-75  mx-auto text-muted">
+        <p class="text-center text-muted">&copy; 2022 PlayTech, Inc</p>
+    </footer>
+</div>
