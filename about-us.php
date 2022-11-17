869da6b083d8b5450f129c343b8ffe6ca3e83676
IkramMahfoud <ikrammahfoud9nemo@gmail.com>
2022-11-17 16:52:37 +0100

    add about-us page

---
+++ about-us.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>PlayTech</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once "includes/etc/css-cdn.php"; ?>
+    <link rel="stylesheet" href="./src/css/about-us.css">
+</head>
+
+<body class="bg-dark">
+    <?php require_once "includes/navbar.php" ?>
+    <div class="img-fluid">
+        <img class="test" src="./assets/image/PS4.jpg" alt="" class="img-fluid">
+    </div>
+    <!-- <div class="testt">
+    <img src="./assets/image/destiny.jpg" class="img-fluid">   
+    </div> -->
+
+    <div class="left">
+        <div class="cards">
+            <div class="text">
+                <h5 class="h2 text-center text-white mb-4 mb-sm-5" class="text-light">About Us.</h5>
+                <p class="font-weight-light" class="text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ipsa,
+                    eos rerum ad vel cupiditate voluptatem quas <br>
+                    dolorem voluptatum assumenda tenetur.
+                    Accusantium magnam quidem quisquam nesciunt facilis dolores perspiciatis eligendi?</p>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./assets/image/destiny.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                    <h5>We Game</h5>
+                    </p>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-warning">Read more</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./assets/image/A-.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                    <h5>We Game</h5>
+                    </p>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-warning">Read more</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./assets/image/digital.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                    <h5>We Game</h5>
+                    </p>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-warning">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
