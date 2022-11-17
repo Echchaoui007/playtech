eff9050cb48a955a78a61d0218846c66dafd308e
6-ON <aminhatim6@gmail.com>
2022-11-17 23:57:28 +0100

    start working on dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,17 @@
+<?php
+$isAdmin = false; // for testing purposes
+if ($isAdmin) {
+} else {
+    header("HTTP/1.1 403 Forbidden");
+    require_once "../includes/etc/error.php";
+    die(403);
+}
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,46 +23,48 @@
     <?php require_once "../includes/etc/css-cdn.php" ?>
 </head>
 
-<body>
+<body class="bg-dark bg-opacity-75">
     <?php require_once "../includes/navbar.php" ?>
-    
-    <h1 class="h1 text-center">Dashboard</h1>
 
-    
-    
+
+    <div class="text-light container text-center my-5">
+        <h2 class="h2">Products Manager</h2>
+
+    </div>
+    <div class="container-sm d-flex justify-content-center mb-2 ">
+        <div class="btn-group " role="group" aria-label="Basic mixed styles example">
+            <button type="button" class="btn btn-success btn">Add</button>
+            <button type="button" class="btn btn-warning">Edit</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+    </div>
     <div class="table-responsive mx-md-5">
-        <table class="table table-striped
-        table-hover	
-        table-borderless
-        table-primary
-        align-middle">
-            <thead class="table-light">
+        <table class="table table-dark table-hover ">
+            <thead>
                 <tr>
-                    <th>Column 1</th>
-                    <th>Column 2</th>
-                    <th>Column 3</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Label</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Image</th>
                 </tr>
-                </thead>
-                <tbody class="table-group-divider">
-                    <tr class="table-primary" >
-                        <td scope="row">Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
+            </thead>
+            <tbody>
+                <?php foreach ($variable as $key => $value) {
+                ?>
+                    <tr class="">
+                        <td scope="row">R1C1</td>
+                        <td>R1C2</td>
+                        <td>R1C3</td>
                     </tr>
-                    <tr class="table-primary">
-                        <td scope="row">Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    
-                </tfoot>
+                <?php } ?>
+            </tbody>
         </table>
     </div>
-    
+
     <?php require_once "../includes/etc/js-cdn.php" ?>
-    <script src="../src/js/test.js"></script>
+    <script src="../src/js/dashboard.js"></script>
 </body>
 
 </html>
