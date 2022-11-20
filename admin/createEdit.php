31ebd0a45072824b08c310487d7411ef926b3a07
6-ON <aminhatim6@gmail.com>
2022-11-20 02:20:18 +0100

    add uploading function (needs improvment)

--- admin/createEdit.php
+++ admin/createEdit.php
@@ -1,5 +1,12 @@
 <?php
+if (isset($_POST["submit"])) {
+    require "uploads.php";
+    uploadProdImg($_FILES["myfile"]);
+}else{
+    echo "you"; 
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,10 +16,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Editor</title>
+    <?php require_once "../includes/etc/css-cdn.php" ?>
 </head>
 
 <body>
     <h1>To implements</h1>
+    <form action="createEdit.php" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="myfile" class="form-label">Choose file</label>
+            <input type="file" class="form-control" name="myfile" id="myfile" placeholder="" aria-describedby="fileHelpId">
+            <div id="fileHelpId" class="form-text">Help text</div>
+        </div>
+        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </body>
 
 </html>
