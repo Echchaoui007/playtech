a94f46dd528994a649d6d0db9604a52b400968f4
Echchaoui <kurra9904@gmail.com>
2022-11-21 16:54:40 +0100

    shop dynamic data

--- shop.php
+++ shop.php
@@ -1,3 +1,20 @@
+<?php
+
+use LDAP\Result;
+
+include "database/connection.php";
+
+$result = $conn->query("select * from product");
+$products = $result->fetch_all(MYSQLI_ASSOC);
+$conn->close();
+
+?>
+
+
+
+
+
+
 <!doctype html>
 <html lang="en">
 
@@ -7,167 +24,45 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <?php require_once "includes/etc/css-cdn.php"; ?>
   <link rel="stylesheet" href="./src/css/shop.css">
+  <style>
+    h4.prix::after {
+      content: " $";
+    }
+  </style>
 </head>
 
 <body class="bg-dark">
 
 
   <?php require_once "includes/navbar.php" ?>
- 
 
 
- 
+
+
 
 
 
   <section class="container mt-5  rounded-3" id="popular-products">
     <h2 class="h2 text-center text-white mb-4 mb-sm-5">Product</h2>
     <div class="row">
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
-          <img src="assets/image/razer-viper-8khz.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Viper 8KHz</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-          <img src="https://assets.razerzone.com/eeimages/support/products/1689/1689-huntsmanmini.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Huntsman Mini</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
+      <?php foreach ($products as $prod) {   ?>
 
-          <img src="assets/image/COOLER-MASTER-MH630.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Cooler Master MH-630</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
+        <div class="col-md mb-3">
+          <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
+            <img src="download/<?php echo $prod["prod_img"] ?>" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title"><?php echo $prod["prod_lib"] ?></h5>
+              <h4 class="prix"><?php echo $prod["price"] ?></h4>
+              <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
-          <img src="assets/image/razer-viper-8khz.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Viper 8KHz</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-          <img src="https://assets.razerzone.com/eeimages/support/products/1689/1689-huntsmanmini.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Huntsman Mini</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-
-          <img src="assets/image/COOLER-MASTER-MH630.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Cooler Master MH-630</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
-          <img src="assets/image/razer-viper-8khz.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Viper 8KHz</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-          <img src="https://assets.razerzone.com/eeimages/support/products/1689/1689-huntsmanmini.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Huntsman Mini</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-
-          <img src="assets/image/COOLER-MASTER-MH630.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Cooler Master MH-630</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
-          <img src="assets/image/razer-viper-8khz.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Viper 8KHz</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-          <img src="https://assets.razerzone.com/eeimages/support/products/1689/1689-huntsmanmini.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Huntsman Mini</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-
-          <img src="assets/image/COOLER-MASTER-MH630.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Cooler Master MH-630</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto " style="width: 18rem;">
-          <img src="assets/image/razer-viper-8khz.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Viper 8KHz</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-          <img src="https://assets.razerzone.com/eeimages/support/products/1689/1689-huntsmanmini.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Razer Huntsman Mini</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md mb-3">
-        <div class="card bg-dark text-bg-dark bg-gradient mx-auto" style="width: 18rem;">
-
-          <img src="assets/image/COOLER-MASTER-MH630.png" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <h5 class="card-title">Cooler Master MH-630</h5>
-            <a href="shop.php" class="btn btn-outline-secondary mt-4">Buy now</a>
-          </div>
-        </div>
-      </div>
+      <?php } ?>
     </div>
+
+
+
   </section>
 
 
